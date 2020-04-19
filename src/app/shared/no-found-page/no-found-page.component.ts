3b5e4d65d3710fc61b5e3500d81dd162c00c8e3c
Yola Aguilar <honinse@gmx.com>
2020-04-20 01:34:16 +0200

    first commit: several pages and routes init.  Only one module

---
+++ src/app/shared/no-found-page/no-found-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-no-found-page',
+  templateUrl: './no-found-page.component.html',
+  styles: []
+})
+export class NoFoundPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
